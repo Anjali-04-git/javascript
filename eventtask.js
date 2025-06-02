e81d3c68c23b0a964935f9992be721ba5ef55637
Anjali-04-git <anjali.shivi.nair04@gmail.com>
2025-06-02 18:02:54 +0530

    js1

---
+++ eventtask.js
@@ -0,0 +1,24 @@
+function fun(){
+    alert("take inputs")
+    let a=parseInt(prompt("enter a number"))
+    let b=parseInt(prompt("enter a number"))
+    console.log(a+b)
+}
+function Red(){
+    document.body.style.backgroundColor = "red"
+}
+function blue(){
+    document.body.style.backgroundColor = "blue"
+}
+function green(){
+    document.body.style.backgroundColor = "green"
+}
+function yellow(){
+    document.body.style.backgroundColor = "yellow"
+}
+
+function color(cl){
+   document.body.style.backgroundColor = "cl"
+
+}
+
